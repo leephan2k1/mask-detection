fc2dc212cced4933bf2f4599e6b92d7da2b4a247
leephan2k1 <pttl2k1@gmail.com>
2021-10-11 17:53:35 +0700

    5th commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,23 @@ export default function Footer() {
         <div className="row">
           <div className="col l-3 m-6 c-12">
             <img className="author" src="./images/lee.jpg" alt="lee" />
+            <p>Triết Lý</p>
             <p className="author-desc">Dev, Slide content</p>
           </div>
 
           <div className="col l-3 m-6 c-12">
             <img className="author" src="./images/ngan.jpg" alt="ngan" />
+            <p>Thiện Ngân</p>
             <p className="author-desc">Voice actor, Present</p>
           </div>
           <div className="col l-3 m-6 c-12">
             <img className="author" src="./images/Khiem.jpg" alt="khiem" />
+            <p>Trọng Khiêm</p>
             <p className="author-desc">Slide design</p>
           </div>
           <div className="col l-3 m-6 c-12">
             <img className="author" src="./images/Nhan.jpg" alt="Nhan" />
+            <p>Trọng Nhân</p>
             <p className="author-desc">Slide content / design</p>
           </div>
         </div>
